74a63824e74f5239bf92d3eb470b2f34c3be0c25
Refaat07 <m_refaat1612@yahoo.com>
2024-03-22 21:36:17 +0200

    Added Boiler-Plate functions for viewing and creating projects.

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path , include
 
-from projects.views import entry_point
+from . import views
 
 urlpatterns = [
-    path('', entry_point, name='entry_point'),
+    path('', views.entry_point, name='entry_point'),
+    path('all', views.viewProjects, name='viewProjects'),
+    path('createNew', views.viewProjects, name='createProject')
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,21 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.urls import reverse
+# from django.contrib.auth.models import Project
 
 
 # Create your views here.
 
 def entry_point(request):
     return render(request, 'projects/entry_point.html')
+
+#Show all Projects
+def viewProjects(request):
+#     projects = Project.objects.all()
+#     return render(request, 'projects/viewProjects.html', {'projects':projects})
+    pass 
+
+#Create new Project
+def createProject(request):
+    pass
+
