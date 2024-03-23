5d8a7af85dd255dc16d8277395fb26dd4a1ec05b
zaynabsabry <zeinabsabry340@gmail.com>
2024-03-23 13:19:51 +0200

    adjust custom_user model

--- users/forms.py
+++ users/forms.py
@@ -2,11 +2,15 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from django.db import models
+from django.utils import timezone
 import re
 
 class CustomUser(User):
     user_image = models.ImageField(upload_to='users/images/', blank=True)
-    phone_number = models.CharField(max_length=15, blank=True)
+    phone_number = models.CharField(max_length=15, null=True)
+    birth_date = models.DateField(null=True, default=timezone.now)
+    facebook = models.CharField(max_length=255, null=True)
+    country = models.CharField(max_length=20, null=True)
 
 class UserModelForm(forms.ModelForm):
     confirm_password = forms.CharField(widget=forms.PasswordInput())
