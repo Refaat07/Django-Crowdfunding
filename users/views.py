d96ed154a59270cc4d5444afca6a8193086fc9fd
zaynabsabry <zeinabsabry340@gmail.com>
2024-03-29 15:27:14 +0200

    added edit project button and adjusted some functionalities in profile

--- users/views.py
+++ users/views.py
@@ -11,14 +11,14 @@ from django.core.mail import EmailMessage
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from .tokens import account_activation_token
+from django.http import HttpResponseServerError
+from django.utils import timezone
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import login as auth_login, logout as auth_logout
 from projects.views import entry_point, homepage 
-# from django.contrib.auth.models import User
 from .forms import CustomUser
 from projects.models import Project
 from .forms import UserEditForm
-
-
-
 
 
 # Create your views here.
@@ -37,8 +37,6 @@ def create_user(request):
             user.save()
             activateEmail(request, user, form.cleaned_data.get('email'))
             return redirect('homepage')
-            # url = reverse("login")
-            # return redirect(url)
         else:
             for error in list(form.errors.values()):
                 messages.error(request, error)
@@ -79,67 +77,12 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        # user.image.save(f'{last_user.pk}.jpg', image_file)
         messages.success(request, "Thank you for your email confirmation. Now you can login your account.")
         return redirect('/users/login')
     else:
         messages.error(request, "Activation link is invalid!")
 
     return redirect('homepage')
-
-
-# def profile(request):
-#      user = request.user
-#      return render(request, 'profile.html', {'user': user})
-
-# def edit_profile(request):
-#     if request.method == 'POST':
-#         user_id = request.user.id
-#         user = CustomUser.objects.get(pk=user_id)
-#         form = UserEditForm(request.POST, request.FILES, instance=user)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('profile')
-#     else:  # Handle the GET request separately
-#         user_id = request.user.id
-#         user = CustomUser.objects.get(pk=user_id)
-#         form = UserEditForm(request.POST, instance=user)  # Initialize the form without POST data
-#     return render(request, 'users/edit_form.html', {'form': form})
-
-
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth import login as auth_login, logout as auth_logout
-from django.http import HttpResponseServerError
-
-# def edit_profile(request):
-#     user_id = request.user.id
-#     user = CustomUser.objects.get(pk=user_id)
-
-#     if request.method == 'POST':
-#         form = UserEditForm(request.POST, request.FILES, instance=user)
-#         if form.is_valid():
-#             try:
-#                 # Update user data
-#                 user = form.save(commit=False)
-#                 if form.cleaned_data['password']:
-#                     user.password = make_password(form.cleaned_data['password'])
-#                 user.save()
-
-#                 # Update session data if necessary
-#                 if request.user.is_authenticated:
-#                     auth_login(request, user)
-
-#                 return redirect('profile')
-#             except Exception as e:
-#                 # Log the error for debugging
-#                 print(f"An error occurred while saving the form: {e}")
-#                 return HttpResponseServerError("An error occurred while saving the form.")
-#     else:
-#         form = UserEditForm(instance=user)
-
-#     return render(request, 'users/edit_form.html', {'form': form})
-
-from django.utils import timezone
 
 
 def edit_profile(request):
@@ -152,7 +95,6 @@ def edit_profile(request):
         form = UserEditForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             try:
-                # Update user data
                 user = form.save(commit=False)
                 if form.cleaned_data['password']:
                     user.password = make_password(form.cleaned_data['password'])
@@ -163,14 +105,13 @@ def edit_profile(request):
                 if not form.cleaned_data['birth_date']:
                     user.birth_date = now
                 user.save()
+                messages.success(request,"Profile updated successfully")
 
-                # Update session data if necessary
                 if request.user.is_authenticated:
                     auth_login(request, user)
 
-                return redirect('profile')
+                return redirect('user_profile')
             except Exception as e:
-                # Log the error for debugging
                 print(f"An error occurred while saving the form: {e}")
                 return HttpResponseServerError("An error occurred while saving the form.")
     else:
@@ -185,10 +126,7 @@ def user_profile(request):
     user_id = request.user.id
     
     user = CustomUser.objects.get(pk=user_id)
-    # print(user.)
     image_url = user.user_image.url if user and user.user_image else None
- 
-    # Pass the image link to the HTML template
     return render(request, 'users/profile.html', {'user': user, 'image_url':image_url})
 
 @login_required(login_url='/users/login')
@@ -200,20 +138,11 @@ def delete_profile(request):
     else:
         return HttpResponse(status=405)
 
+from django.forms.models import model_to_dict
 def user_projects(request):
     user_projects = Project.objects.filter(id=request.user.id)
+    project_list = [model_to_dict(project) for project in user_projects]
+    print(project_list)
 
-    return render(request, 'users/user_projects.html', {'user_projects': user_projects})
+    return render(request, 'users/user_projects.html', {'user_projects': project_list})
     
-
-
-# def home_index(request):
-#     user_id = request.user.id
-    
-#     user = CustomUser.objects.get(pk=user_id)
-#     # print(user.)
-#     image_url = user.user_image.url if user and user.user_image else None
-#     print(image_url)
-    
-#     # Pass the image link to the HTML template
-#     return render(request, 'projects\entry_point.html', {'image_url':image_url})
